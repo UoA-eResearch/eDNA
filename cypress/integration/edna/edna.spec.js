90fbc23fd5febd06ef77d67926bb52106078424b
warrick <wcor690@aucklanduni.ac.nz>
2019-05-13 10:25:54 +1200

    adjusted tests for different in response data given.

--- cypress/integration/edna/edna.spec.js
+++ cypress/integration/edna/edna.spec.js
@@ -55,9 +55,9 @@ describe("Colour metric select", function() {
   });
 
   it("should change circle colour on selection change", function() {
-    cy.get("#_919:first").should("have.attr", "fill", "rgb(0, 0, 255)");
+    cy.get("#_359:first").should("have.attr", "fill", "rgb(0, 0, 255)");
     cy.get("#meta-select").select("biome_t2");
-    cy.get("#_919:first").should("have.not.attr", "fill", "rgb(0, 0, 255)");
+    cy.get("#_359:first").should("have.not.attr", "fill", "rgb(0, 0, 255)");
   });
 });
 
@@ -72,20 +72,20 @@ describe("Colour scheme select", function() {
 
   it("should change colour for certain metrics", function() {
     cy.get("#meta-select").select("elevation");
-    cy.get("#_919:first").should("have.attr", "fill", "rgb(0, 0, 255)");
+    cy.get("#_359:first").should("have.attr", "fill", "rgb(0, 0, 255)");
     cy.get("#colour-scheme-select").select("diverging");
-    cy.get("#_919:first").should("have.not.attr", "fill", "rgb(0, 0, 255)");
+    cy.get("#_359:first").should("have.not.attr", "fill", "rgb(0, 0, 255)");
   });
 
   it("should not alter colour for specified metrics", function() {
     cy.get(".leaflet-control-layers-overlays input:first").click();
     cy.get("#meta-select").select("biome_t2");
     cy.wait(400);
-    cy.get("#_919:first").then(circleElem => {
+    cy.get("#_359:first").then(circleElem => {
       const firstFill = Cypress.$(circleElem).attr("fill");
       cy.get("#colour-scheme-select").select("diverging");
       cy.wait(400);
-      cy.get("#_919:first").should("have.attr", "fill", firstFill);
+      cy.get("#_359:first").should("have.attr", "fill", firstFill);
     });
   });
 });
@@ -134,11 +134,14 @@ context("Sidebar", () => {
 
 context("Segmented OTU selects", () => {
   before(() => {
-    cy.visit("/");
+    // cy.visit("/");
+    // cy.get('a[data-cy="search-tab"]').click();
   });
 
   beforeEach(() => {
+    cy.visit("/");
     cy.get('a[data-cy="search-tab"]').click();
+    // cy.get('a[data-cy="search-tab"]').click();
     // cy.get('.leaflet-sidebar-tabs a[data-cy="search-tab]"').click();
   });
 
@@ -194,14 +197,7 @@ context("Segmented OTU selects", () => {
     );
   });
 
-  it.only("Should add current taxon selections to combination select when add button pressed.", () => {
-    // cy.get("#_919:first").then(circleElem => {
-    //   const firstFill = Cypress.$(circleElem).attr("fill");
-    //   cy.get("#colour-scheme-select").select("diverging");
-    //   cy.wait(400);
-    //   cy.get("#_919:first").should("have.attr", "fill", firstFill);
-    // });
-
+  it("Should add current taxon selections to combination select when add button pressed.", () => {
     cy.get("#select2-kingdomSelect-container").click();
     cy.get(".select2-results__options").contains("Animalia");
     cy.get(
