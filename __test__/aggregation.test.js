e1d2b06e4acdf86dc6f8b33aeb44fd4cb3facda5
warrick <wcor690@aucklanduni.ac.nz>
2019-05-27 13:39:34 +1200

    Extracted aggregation functions to be jest testable.

---
+++ __test__/aggregation.test.js
@@ -0,0 +1,8 @@
+import { aggregateSampleOtusBySite } from "../src/aggregation";
+
+// require aggregateSampleOtusBySite
+
+test("Aggregating Sample Otu entries into a summed values", () => {
+  //   let hi = [1, 2];
+  aggregateSampleOtusBySite(null);
+});
--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ import {
   initContextSelect,
   initContextFieldSelect
 } from "./selects";
+import {
+  aggregateSampleOtusBySite,
+  aggregateSamplesByCell
+} from "./aggregation";
 
 window.circles = [];
 window.contextTags = [];
@@ -83,13 +87,13 @@ export function createAggregateUrl() {
     url += "&operator=intersection";
   }
   console.log("request url: " + url);
-  fetchSampleOtus(url);
+  requestSampleOtus(url);
 }
 
 /**
  * Gets sampleOtu data and triggers for recalculations on response
  */
-const fetchSampleOtus = url => {
+const requestSampleOtus = url => {
   // fetch the url
   showLoadingMessage();
   fetch(url).then(response => {
@@ -197,137 +201,6 @@ function addLayerIdToSampleContext(layerGroup) {
   });
 }
 
-/**
- * Iterates the sample otu json response and sums the values by site
- * @param {*} sampleOtus
- */
-function aggregateSampleOtusBySite(sampleOtus) {
-  let siteAggs = {};
-  let missingOtus = [];
-  for (let i in sampleOtus) {
-    let tuple = sampleOtus[i];
-    let otuId = tuple[0];
-    let siteId = tuple[1];
-    let abundance = tuple[2];
-    // if site doesn't exist set up blank site and always increment to avoid messy conditionals.
-    if (!(siteId in siteAggs)) {
-      siteAggs[siteId] = {
-        abundance: 0,
-        richness: 0,
-        otus: {}
-      };
-    }
-    let siteAgg = siteAggs[siteId];
-    siteAgg.abundance += abundance;
-    // same default object creation principle applied to the otus sub-object.
-    if (!(otuId in siteAgg.otus)) {
-      siteAgg.otus[otuId] = {
-        abundance: 0,
-        count: 0
-      };
-      siteAgg.richness++;
-      siteAgg.otus[otuId].abundance += abundance;
-      siteAgg.otus[otuId].count++;
-    }
-  }
-  return siteAggs;
-}
-
-/**
- * Iterates the site aggregates and sums the values by grid cell coordinates. Returns a dictionary which keys indicate positional offsets.
- * @param {*} siteAggs
- * @param {*} sampleContexts
- */
-function aggregateSamplesByCell(siteAggs) {
-  // setting up grid parameters
-  makeGrid(detailLevel);
-  // console.log("aggregate by cell");
-  let start = [164.71222, -33.977509];
-  let end = [178.858982, -49.66352];
-  const hardBounds = L.latLngBounds(start, end);
-  const northWest = hardBounds.getNorthWest();
-  const northEast = hardBounds.getNorthEast();
-  const southWest = hardBounds.getSouthWest();
-  // NOTE: alternative way of calculating is incorrectly plotting. some strange offset.
-  // const northWest = L.latLng(start[0], start[1]);
-  // const northEast = L.latLng(start[0], end[1]);
-  // const southWest = L.latLng(end[0], start[1]);
-  const latOffset = (northWest.lat - southWest.lat) / detailLevel;
-  const lngOffset = (northEast.lng - northWest.lng) / detailLevel;
-  // using the params for generating the keys
-  let cellAggs = {};
-  for (let siteId in siteAggs) {
-    let site = siteAggs[siteId];
-    let sampleContext = window.sampleContextLookup[siteId];
-    let x = sampleContext.longitude;
-    let y = sampleContext.latitude;
-    let cellKey = generateCellKey(x, y, start, lngOffset, latOffset);
-    // if doesn't exist, create the cell.
-    if (!(cellKey in cellAggs)) {
-      cellAggs[cellKey] = {
-        abundance: 0,
-        richness: 0,
-        sites: [],
-        otus: {},
-        coordinates: calculateCellCoordinates(
-          cellKey,
-          start,
-          lngOffset,
-          latOffset
-        )
-      };
-    }
-    // Adding values that are allowed to overlap/accumulate.
-    let cell = cellAggs[cellKey];
-    cell.abundance += site.abundance;
-    cell.sites.push(siteId);
-    for (let otuId in site.otus) {
-      let siteOtu = site.otus[otuId];
-      if (!(otuId in cell.otus)) {
-        cell.otus[otuId] = {
-          abundance: 0,
-          count: 0
-        };
-        // alternatively just use the otus keys length and assign to richness.
-        cell.richness++;
-      }
-      let cellOtu = cell.otus[otuId];
-      cellOtu.abundance += siteOtu.abundance;
-      cellOtu.count += siteOtu.count;
-    }
-    for (let key in cellAggs) {
-      let cellAgg = cellAggs[key];
-      cellAgg["siteCount"] = cellAgg.sites.length;
-    }
-  }
-  return cellAggs;
-
-  function calculateCellCoordinates(key, start, latOffset, lngOffset) {
-    // can use the key + grid start to reverse engineer the coordinates
-    let offsets = parseInt(key);
-    let yFactor = Math.floor(offsets / detailLevel);
-    let xFactor = offsets % detailLevel;
-    let cellStartX = start[0] + lngOffset * xFactor;
-    let cellStartY = start[1] - latOffset * yFactor;
-    // order: topleft, topright, bottomright, bottomleft
-    return [
-      [cellStartX, cellStartY],
-      [cellStartX + lngOffset, cellStartY],
-      [cellStartX + lngOffset, cellStartY - latOffset],
-      [cellStartX, cellStartY - latOffset]
-    ];
-  }
-
-  function generateCellKey(x, y, start, latOffset, lngOffset) {
-    let lngDiff = Math.abs(x) - Math.abs(start[0]);
-    let colIndex = Math.floor(lngDiff / lngOffset);
-    let latDiff = Math.abs(y) - Math.abs(start[1]);
-    let rowIndex = Math.floor(latDiff / latOffset);
-    let cellKey = rowIndex * detailLevel + colIndex;
-    return cellKey;
-  }
-}
-
 function makeFeatureCollection(cellAggs) {
   let maxes = calculateMaxes(cellAggs);
   let featureCollection = {
@@ -397,74 +270,6 @@ function hideLoadingMessage() {
 }
 
 /**
- * Makes grid array for cells. Each cell starts off with only containing coordinates.
- * @param {*} detailLevel
- */
-function makeGrid(detailLevel) {
-  //Hard coded bounds and offsets.
-  const gridStart = [164.71222, -33.977509];
-  const gridEnd = [178.858982, -49.66352];
-
-  const hardBounds = L.latLngBounds(gridStart, gridEnd);
-  let northWest = hardBounds.getNorthWest();
-  let northEast = hardBounds.getNorthEast();
-  let southWest = hardBounds.getSouthWest();
-  const latOffset = (northWest.lat - southWest.lat) / detailLevel;
-  const lngOffset = (northEast.lng - northWest.lng) / detailLevel;
-  // console.log(latOffset, lngOffset);
-  // The bounds method seems to make the rectangle less distorted
-  // const latOffset = (gridStart[1] - gridEnd[1]) / detailLevel;
-  // const lngOffset = (gridEnd[0] - gridStart[0]) / detailLevel;
-
-  let gridCells = [];
-  let cellStart = gridStart;
-  for (let i = 0; i < detailLevel; i++) {
-    for (let j = 0; j < detailLevel; j++) {
-      //create rectangle polygon.
-      const cell = makeCell();
-      gridCells.push(cell);
-      cellStart = incrementLongitude();
-    }
-    cellStart = resetLongitudeDecrementLatitude();
-  }
-
-  let grid = {
-    start: gridStart,
-    lngOffset: lngOffset,
-    latOffset: latOffset,
-    detailLevel: detailLevel,
-    cells: gridCells
-  };
-  return grid;
-
-  function incrementLongitude() {
-    return [cellStart[0] + lngOffset, cellStart[1]];
-  }
-
-  function resetLongitudeDecrementLatitude() {
-    return [cellStart[0] - lngOffset * detailLevel, cellStart[1] - latOffset];
-  }
-
-  function makeCell() {
-    let topLeft = [cellStart[0], cellStart[1]];
-    let topRight = [cellStart[0] + lngOffset, cellStart[1]];
-    let bottomRight = [cellStart[0] + lngOffset, cellStart[1] - latOffset];
-    let bottomLeft = [cellStart[0], cellStart[1] - latOffset];
-    let cell = [topLeft, topRight, bottomRight, bottomLeft];
-    cell = {
-      coordinates: cell,
-      abundance: 0,
-      richness: 0,
-      cellSpecies: {},
-      cellSites: [],
-      hasSamples: false
-    };
-    // console.log(cell.coordinates);
-    return cell;
-  }
-}
-
-/**
  * Adds additional metrics to site aggregated data.
  */
 function calculateSiteMetrics(siteAggregates) {
@@ -513,7 +318,7 @@ const tileLayer = L.tileLayer(
   }
 );
 
-export const map = initMap();
+const map = initMap();
 function initMap() {
   let map = L.map("map", {
     zoomSnap: 0.25,
@@ -537,7 +342,7 @@ function initMap() {
 var taxonParams = new URLSearchParams(window.location.search);
 var mode = taxonParams.get("mode");
 
-var detailLevel = 60;
+let detailLevel = 60;
 //warrick map additions
 
 //shows the scale of the map
@@ -980,7 +785,7 @@ const initSubmitSearch2Button = () => {
     let slug = params.join("&");
     let url = API_URLS.sampleOtus + slug;
     // console.log(url);
-    fetchSampleOtus(url);
+    requestSampleOtus(url);
   };
 };
 
@@ -1023,3 +828,5 @@ fetch(url).then(response => {
   });
 });
 // TODO: fix layer rendering only workng when contextual filter has conditions. Something to do with the backend not returning results when no contextual present.
+
+export { detailLevel, map };
