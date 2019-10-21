13cf9bfe5720f359e04017285fa90ad4e75b542b
warrickct <wcor690@aucklanduni.ac.nz>
2019-10-22 11:08:01 +1300

    added rule for css imports

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   }
